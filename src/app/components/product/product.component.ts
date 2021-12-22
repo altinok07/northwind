274f078e9395ccc922ba394f03aa4482f45af142
HÜSEYİN ALTINOK <huseyin.altinok@std.antalya.edu.tr>
2021-12-23 01:36:00 +0300

    Sepet İşlemi eklendi, Toastr Notification eklendi.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -11,21 +13,23 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  filterText = "";
+  filterText = '';
 
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if (params["categoryId"]) {
-        this.getProductsByCategory(params["categoryId"])
-      }else{
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
         this.getProducts();
-      } 
-    })
+      }
+    });
   }
 
   getProducts() {
@@ -41,5 +45,16 @@ export class ProductComponent implements OnInit {
         this.products = response.data;
         this.dataLoaded = true;
       });
+  }
+
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error("Hata","Bu Ürün Sepete Eklenemez !!")
+    }
+    else{
+      this.toastrService.success("Sepete Eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }
+
   }
 }
